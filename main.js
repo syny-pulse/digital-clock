ee7f8eae73a3044d12b6d7aa3e6c01abce0ee479
syny-pulse <sinaijose002@gmail.com>
2023-07-23 20:50:21 +0300

    it worked

---
+++ main.js
@@ -0,0 +1,15 @@
+function updateClock() {
+    const clockElement = document.getElementById('clock');
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    clockElement.textContent = timeString;
+}
+
+// Update the clock every second
+setInterval(updateClock, 1000);
+
+// Initial update when the page loads
+updateClock();
